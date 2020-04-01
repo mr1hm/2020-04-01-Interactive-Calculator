b2b5342d84ba391465cd278768aa0bd9b403d147
Kevin <kevinihm@gmail.com>
2020-04-01 11:15:57 -0700

    imported TipCalculator to index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import TipCalculator from './components/app';
 
 ReactDOM.render(
-  <App />,
+  <TipCalculator />,
   document.querySelector('#tip-calculator')
 );
